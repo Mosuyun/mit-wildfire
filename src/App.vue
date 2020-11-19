b98d65ad88d261ab2f892da7492329df82c7d919
Mosuyun <709963735@qq.com>
2020-11-19 22:34:45 +0800

    initmap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <mymap>HelloWorld</mymap>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import mymap from 'components/Map.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  Components: {
+    mymap,
+  },
 }
 </script>
 
 <style>
+@import 'assets/css/base.css';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1000px;
+  height: 500px;
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="map">test</div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+const mapboxToken =
+  'pk.eyJ1IjoibW9zdXl1biIsImEiOiJjazMxYnZ5Y3UwN201M2RvazN3OXI2MWU0In0.ZF9801IzUETje05jGnrE7w'
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      map: null,
+    }
+  },
+  mounted() {
+    this.initMap()
+  },
+  methods: {
+    initMap: function () {
+      console.log('start')
+      mapboxgl.accessToken = mapboxToken
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-119.32302, 37.40631],
+        zoom: 5.82,
+        pitch: 45.0,
+        bearing: 0.0,
+      })
+      this.nav = new mapboxgl.NavigationControl()
+      this.map.addControl(this.nav)
+      this.map.on('click', this.mapClickEvent)
+    },
+    mapClickEvent: function (e) {
+      console.log('经纬度是', e.lngLat)
+    },
+  },
+}
+</script>
+
+<style>
+#map {
+  width: 1000px;
+  height: 500px;
+  background-color: blueviolet;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
